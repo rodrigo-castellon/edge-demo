8fd30759fb01af0f91337d1943989c6c01c563ad
Rodrigo Castellon <rjcaste@stanford.edu>
2022-12-15 04:05:14 -0800

    firebase queue + gcp cloud stuff scaffolded

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
+/**
+ * The general architecture is roughly as follows:
+ * - The client sends a request to the server to request a song
+ * - The server checks the queue (Firebase) and if the queue is too long, it
+ *  sends a response to the client saying that the queue is too long,
+ *  and the client should try again later.
+ * - If the queue is not too long, then the server sends a response
+ *  to the client saying that the queue is not too long, and that
+ *  the song has been placed into the queue. It also updates the Firebase
+ *  database so that the queue has this song at the end. Since every client
+ *  is viewing the same Firebase database, every client will see the song
+ *  appear at the end of the queue.
+ * - The server then downloads the song from youtube as an MP3,
+ *  and uploads it to the GCP bucket. On GCP, the bucket is
+ *  configured to trigger a Cloud Function when a new file is
+ *  uploaded.
+ * - The Cloud Function then spins up a VM, and runs a script that runs
+ *  inference on the song, producing a sequence of human dance motions.
+ *  This is then saved to another GCP bucket.
+ * - The client is always keeping track of the queue. As time goes on,
+ *  the client tries to, depending on availability, download the dance
+ *  sequences from the GCP bucket for every song in the queue. On the client
+ *  side, these motions are just progressively stored as files on the device.
+ *  Since everything is synchronized, the client knows when to play each animation,
+ *  and does so.
+ */
+
 const path = require('node:path');
 const express = require("express");
 const webpackDevMiddleware = require("webpack-dev-middleware");
@@ -6,6 +33,29 @@ const webpackConfig = require("./webpack.config.js");
 const url = require('url');
 const youtubedl = require('youtube-dl-exec')
 const {Storage} = require('@google-cloud/storage');
+var admin = require("firebase-admin");
+
+// https://firebase.google.com/docs/database/admin/save-data
+var serviceAccount = require("/Users/rodrigo-castellon/edging/edging-abb31-firebase-adminsdk-8f00j-721d54f56d.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://edging-abb31-default-rtdb.firebaseio.com"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+const queueRef = db.ref('/queue');
+
+queueRef.set({
+    alanisawesome: {
+        date_of_birth: 'June 23, 1912',
+        full_name: 'Alan Turing'
+    },
+});
+
+// Initialize Realtime Database and get a reference to the service
+// const database = getDatabase(app);
 
 // For more information on ways to initialize Storage, please see
 // https://googleapis.dev/nodejs/storage/latest/Storage.html
@@ -15,9 +65,6 @@ const storage = new Storage({keyFilename: 'hai-gcp-dexterous-e53879aeb117.json'}
 
 const MP3_DIR = 'mp3s';
 
-/**
- * TODO(developer): Uncomment these variables before running the sample.
- */
 // The ID of your GCS bucket
 const ingestionBucketName = 'song-ingestion';
 
@@ -66,21 +113,9 @@ app.get('/about', function(req, res) {
             res.status(500).send(err)
         }
     })
-})
+});
 
-app.post('/api/request_song', function(req, res) {
-    // the user wants to request a song to be put onto
-    // the queue
-
-    // the request contains a youtube link, so we need to
-    // parse the link and get the video id
-
-    const queryObject = url.parse(req.url, true).query;
-
-    const link = queryObject.link;
-
-    // make request to youtube api to download the video
-    // as an mp3 file
+function uploadSongToGCP(videoId) {
     youtubedl(link, {
         x: true,
         o: MP3_DIR + "/%(id)s.%(ext)s",
@@ -95,10 +130,9 @@ app.post('/api/request_song', function(req, res) {
     }).then(output => {
         // now we need to send the mp3 file to the google cloud
         // storage bucket
-        const video_id = link.split('v=')[1];
 
         uploadFile(
-            MP3_DIR + '/' + video_id + '.mp3', video_id + '.mp3'
+            MP3_DIR + '/' + videoId + '.mp3', videoId + '.mp3'
         ).then(() => {
             console.log("uploaded file to google cloud storage successfully");
             res.send({"status": 200, "message": "success"});
@@ -110,6 +144,68 @@ app.post('/api/request_song', function(req, res) {
         console.error(err);
         res.send({"status": 500, "message": "failed on download"});
     });
+}
+
+function addSongToQueue(videoId, res) {
+    return new Promise((resolve, reject) => {
+        queueRef.set({
+            videoId: videoId
+        }, (error) => {
+            if (error) {
+                reject(error);
+            } else {
+                res.send({"status": 200, "message": "song added to queue"});
+                resolve();
+            }
+        });
+    });
+}
+
+
+function handleLink(link, res) {
+    const videoId = link.split('v=')[1];
+
+    // check against firebase queue. if the queue is too long,
+    // then we need to send a message to the client saying that
+    // the queue is too long, and that they need to wait for
+    // a while before requesting another song
+    queueRef.on('value', (snapshot) => {
+        const queueLength = snapshot.numChildren();
+        console.log("queue length is:" + queueLength);
+        if (queueLength > 10) {
+            res.send({"status": 500, "message": "queue is too long"});
+        } else {
+            addSongToQueue(
+                videoId, res
+            ).then(() => uploadSongToGCP(videoId)
+            ).catch((err) => {
+                console.log(err);
+                res.send({"status": 500, "message": "error adding song to queue"});
+            });
+        }
+    }).catch((err) => {
+        console.log(err);
+        res.send({"status": 500, "message": "error checking queue length"});
+    });
+};
+
+app.post('/api/request_song', function(req, res) {
+    // the user wants to request a song to be put onto
+    // the queue
+
+    // the request contains a youtube link, so we need to
+    // parse the link and get the video id
+
+    const queryObject = url.parse(req.url, true).query;
+    const link = queryObject.link;
+    const videoId = link.split('v=')[1];
+
+    // check against firebase queue. if the queue is too long,
+    // then we need to send a message to the client saying that
+    // the queue is too long, and that they need to wait for
+    // a while before requesting another song
+
+    handleLink(link, res);
 });
 
 const server = app.listen(3000, function() {
