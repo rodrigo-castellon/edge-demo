684ce2a69c6cee7cfd11cba5a9d4ded83e2f31e1
Rodrigo Castellon <rjcaste@stanford.edu>
2022-12-15 00:00:19 -0800

    more re-organization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// css
+// import './App.css';
+
 // custom components
 import NavbarCustom from "./components/NavbarCustom";
 
---
+++ src/components/Display.js
@@ -0,0 +1,41 @@
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+import { Ground } from './Ground';
+import { Hiphop } from './Hiphop';
+
+export default function Display() {
+    return (
+        <Suspense fallback={null}>
+            <Canvas shadows camera={{ fov: 75, position: [-2, 4, 4], rotation: [30,0,0]}}>
+                <color args={[0, 0, 0]} attach="background" />
+                <spotLight
+                    color={[1, 0.25, 0.7]}
+                    intensity={1.5}
+                    angle={0.6}
+                    penumbra={0.5}
+                    position={[5, 5, 0]}
+                    castShadow
+                    shadow-bias={-0.0001}
+                />
+                <spotLight
+                    color={[0.14, 0.5, 1]}
+                    intensity={2}
+                    angle={0.6}
+                    penumbra={0.5}
+                    position={[-5, 5, 0]}
+                    castShadow
+                    shadow-bias={-0.0001}
+                />
+                <ambientLight intensity={0.5} />
+                <Ground />
+                <OrbitControls 
+                    target={[0, 0.35, 0]}
+                    maxPolarAngle={1.45}
+                />
+                <Hiphop />
+            </Canvas>
+        </Suspense>
+    );
+}
