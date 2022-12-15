502c5db5fc2608444dc15ee893c5441eb94efa92
Rodrigo Castellon <rjcaste@stanford.edu>
2022-12-14 18:12:30 -0800

    progress (favicon + better rendering)

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(
 );
 
 app.use(express.static(__dirname + "/www"));
+app.use(express.static(__dirname + "/public"));
 
 const server = app.listen(3000, function() {
   const host = server.address().address;
--- src/App.js
+++ src/App.js
@@ -10,9 +10,62 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { useRef, useState, useEffect, Suspense } from 'react'
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
+import { OrbitControls, MeshReflectorMaterial, Stats } from '@react-three/drei'
+import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
+
+export function Ground() {
+    // thanks to https://polyhaven.com/a/rough_plasterbrick_05 !
+    // const [roughness, normal] = useLoader(TextureLoader, [
+    //   process.env.PUBLIC_URL + "textures/terrain-roughness.jpg",
+    //   process.env.PUBLIC_URL + "textures/terrain-normal.jpg",
+    // ]);
+  
+    // useEffect(() => {
+    //   [normal, roughness].forEach((t) => {
+    //     t.wrapS = RepeatWrapping;
+    //     t.wrapT = RepeatWrapping;
+    //     t.repeat.set(5, 5);
+    //     t.offset.set(0, 0);
+    //   });
+  
+    //   normal.encoding = LinearEncoding;
+    // }, [normal, roughness]);
+  
+    // useFrame((state, delta) => {
+    //   let t = -state.clock.getElapsedTime() * 0.128;
+    //   roughness.offset.set(0, t % 1);
+    //   normal.offset.set(0, t % 1);
+    // });
+  
+    return (
+      <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
+        <planeGeometry args={[30, 30]} />
+        <MeshReflectorMaterial
+          envMapIntensity={0}
+          //normalMap={normal}
+          //normalScale={[0.15, 0.15]}
+          //roughnessMap={roughness}
+          dithering={true}
+          color={[0.015, 0.015, 0.015]}
+          roughness={0.7}
+          blur={[1000, 400]} // Blur ground reflections (width, heigt), 0 skips blur
+          mixBlur={30} // How much blur mixes with surface roughness (default = 1)
+          mixStrength={80} // Strength of the reflections
+          mixContrast={1} // Contrast of the reflections
+          resolution={1024} // Off-buffer resolution, lower=faster, higher=better quality, slower
+          mirror={0} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+          depthScale={0.01} // Scale the depth factor (0 = no depth, default = 0)
+          minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+          maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+          depthToBlurRatioBias={0.25} // Adds a bias factor to the depthTexture before calculating the blur amount [blurFactor = blurTexture * (depthTexture + bias)]. It accepts values between 0 and 1, default is 0.25. An amount > 0 of bias makes sure that the blurTexture is not too sharp because of the multiplication with the depthTexture
+          debug={0}
+          reflectorOffset={0.2} // Offsets the virtual camera that projects the reflection. Useful when the reflective surface is some distance from the object's origin (default = 0)
+        />
+      </mesh>
+    );
+  }
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -38,34 +91,61 @@ function Box(props) {
 }
 
 
+const deg2rad = degrees => degrees * (Math.PI / 180);
+
+
 // ----------------------------------------------------------------------
 function App() {
 
     return (
-      <div>
-          <Navbar bg="dark" variant="dark">
-              <Container>
-                  <Navbar.Brand href="#home">
-                  <img
-                      alt=""
-                      src="https://react-bootstrap.github.io/logo.svg"
-                      width="30"
-                      height="30"
-                      className="d-inline-block align-top"
-                  />{' '}
-                  EDGE: Editable Dance Generation From Music
-                  </Navbar.Brand>
-              </Container>
-          </Navbar>
-          <Canvas>
-            <ambientLight intensity={0.5} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-            <OrbitControls />
-          </Canvas>
-      </div>
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                        <Navbar.Brand href="#home">
+                        <img
+                                alt=""
+                                src="https://react-bootstrap.github.io/logo.svg"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                        />{' '}
+                        EDGE: Editable Dance Generation From Music
+                        </Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Suspense fallback={null}>
+                <Canvas shadows camera={{ fov: 75, position: [-2, 4, 4], rotation: [30,0,0]}}>
+                    <color args={[0, 0, 0]} attach="background" />
+                    <spotLight
+                        color={[1, 0.25, 0.7]}
+                        intensity={1.5}
+                        angle={0.6}
+                        penumbra={0.5}
+                        position={[5, 5, 0]}
+                        castShadow
+                        shadow-bias={-0.0001}
+                    />
+                    <spotLight
+                        color={[0.14, 0.5, 1]}
+                        intensity={2}
+                        angle={0.6}
+                        penumbra={0.5}
+                        position={[-5, 5, 0]}
+                        castShadow
+                        shadow-bias={-0.0001}
+                    />
+                    <ambientLight intensity={0.5} />
+                    <Box position={[-1.2, 0, 0]} />
+                    <Box position={[1.2, 0, 0]} />
+                    <Ground />
+                    <OrbitControls 
+                        target={[0, 0.35, 0]}
+                        maxPolarAngle={1.45}
+                    />
+                    <Stats />
+                </Canvas>
+            </Suspense>
+        </div>
     );
   }
 
