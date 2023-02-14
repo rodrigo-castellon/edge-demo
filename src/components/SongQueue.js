aa4f034a5678ddd91bafbe5276aa2b15f6241764
Rodrigo Castellon <rjcaste@stanford.edu>
2023-02-14 03:30:36 -0800

    display prev cur next

--- src/components/SongQueue.js
+++ src/components/SongQueue.js
@@ -2,12 +2,16 @@ import ListGroup from "react-bootstrap/ListGroup";
 
 export function SongQueue(props) {
     console.log(props.queue);
+
+    const queue = [props.queue[props.queue.length - 1]].concat(
+        props.queue.slice(0, 2)
+    );
     return (
         <ListGroup as="ul">
             <ListGroup.Item as="li" active>
-                Up Next
+                Songs
             </ListGroup.Item>
-            {props.queue.map((item) => (
+            {queue.map((item) => (
                 <ListGroup.Item as="li">{item.split("/")[1]} </ListGroup.Item>
             ))}
         </ListGroup>
